e593b80fef517d59384982f9e2bfaafa33ff6fe8
Daniel Barnard <jok3d.tk@gmail.com>
2022-11-11 19:25:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Pyramid_Builder
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int pyrHeight = int.Parse(Console.ReadLine());
+
+            //Functions 
+            static string BrickLayer(int Bricks)
+            {
+                string layer = "";
+
+                return layer;
+            }
+
+            StringBuilder Pyramid = new StringBuilder(); // build the pyramid
+
+            // append string to Pyramid
+            for(int i = pyrHeight; i > 0; i--)
+            {
+                Pyramid.AppendLine(BrickLayer(i));
+            }
+
+            var outputPyramid = Pyramid.ToString(); // outputs the built pyramid
+        }
+    }
+
+}
+
+// height = 3
+
+// #
+// ##
+// ###
