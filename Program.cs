c36954462da3ce8b872ff998d82e15ad92fdc81a
Daniel Barnard <jok3d.tk@gmail.com>
2022-11-21 04:14:53 +0200

    Extended ui elements. Introduced option to continue creating.

--- Program.cs
+++ Program.cs
@@ -6,16 +6,53 @@ namespace Pyramid_Builder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the size of your shape: ");
-            int pyrHeight = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Please enter the symbol for your shape: ");
-            string pyrChar = Console.ReadLine();
+            // Initialization
+            int pyrHeight;
+            string pyrChar;
+            string userInput;
 
             ShapeBuilder pyramid = new ShapeBuilder();
             BorderBuilder border = new BorderBuilder();
 
-            Console.WriteLine(border.GenerateBorder(pyramid.buildTriangle(pyrHeight, pyrChar)));
+            Console.WriteLine("Enter 'n' to exit the App.\nEnter 'y' to start.");
+            userInput = Console.ReadLine();
+            while (userInput != "n")
+            {
+                // Prompt user for size
+                Console.WriteLine("Please enter the size of your shape: ");
+
+                try
+                {
+                    pyrHeight = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                // Prompt user for symbol
+                Console.WriteLine("Please enter the (1) symbol for your shape: ");
+                pyrChar = Console.ReadLine();
+
+                try
+                {
+                    while (pyrChar.Length != 1)
+                    {
+                        Console.WriteLine("Please enter a valid symbol amount.");
+                        pyrChar = Console.ReadLine();
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                Console.WriteLine(border.GenerateBorder(pyramid.buildTriangle(pyrHeight, pyrChar)));
+                
+                // Prompt User to continue
+                Console.WriteLine("Do you wish to continue? y/n");
+                userInput = Console.ReadLine();
+            }
         }
     }
 }
